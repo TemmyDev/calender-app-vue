b62058e063dde893fe5525288899af51c7de6975
TemmyDev <tlajorin@gmail.com>
2020-08-25 13:19:01 +0100

    second commit

---
+++ src/app/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <CalenderWeek />
+    <CalenderEntry />
+  </div>
+</template>
+
+<script>
+import CalenderWeek from './components/CalenderWeek.vue';
+import CalenderEntry from './components/CalenderEntry.vue';
+export default {
+  name: 'App',
+  components: {
+    CalenderWeek,
+    CalenderEntry
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+   #app {
+  height: inherit;
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+</style>
